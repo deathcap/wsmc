76942b160fac02ca639d55f52ff512fb137affe5
deathcap <deathcap@gmx.co.uk>
2014-01-04 17:45:42 -0800

    Log more packets

--- examples/mcwebchat/index.js
+++ examples/mcwebchat/index.js
@@ -2,20 +2,26 @@ var WebSocket = require('ws');
 var ever = require('ever');
 var tellraw2dom = require('tellraw2dom');
 
+var outputNode = document.getElementById('output');
+var inputNode = document.getElementById('input');
+
+var log = function(s) {
+  outputNode.appendChild(document.createTextNode(s));
+  outputNode.appendChild(document.createElement('br'));
+}
+
 var ws = new WebSocket('ws://localhost:1234');
 console.log('ws',ws);
 ever(ws).on('open', function() {
-  console.log('opened');
-  //ws.send('hey'); // TODO: user input
+  log('Connected to WebSocket');
 });
 
-var outputNode = document.getElementById('output');
-var inputNode = document.getElementById('input');
 
 ever(ws).on('message', function(event, flags) {
   var packet = JSON.parse(event.data);
   var name = packet[0], payload = packet[1];
-  
+
+  // show formatted chat
   if (name === 'chat') {
     console.log(payload);
 
@@ -27,6 +33,14 @@ ever(ws).on('message', function(event, flags) {
         outputNode.appendChild(document.createTextNode('error: ' + error));
       }
     }
+  // log a few other interesting messages
+  } else if (name === 'disconnect') {
+    log('Kicked by server! Reason: ' + payload.reason);
+  } else if (name === 'spawn_position') {
+    log('Spawned at ('+payload.x+','+payload.y+','+payload.z+')');
+  } else if (name === 'player_list_item') {
+    if (payload.online)
+      log(payload.playerName + ' ('+payload.ping+' ms)');
   }
 });
 
@@ -34,7 +48,7 @@ ever(document.body).on('keyup', function(event) {
   if (event.keyCode !== 13) return;
 
   var input = inputNode.value;
-  ws.send(JSON.stringify(['chat_message', {message: input}])); // just send it raw
+  ws.send(JSON.stringify(['chat_message', {message: input}]));
   
   inputNode.value = '';
 });
