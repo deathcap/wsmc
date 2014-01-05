b26920bbe8b71db6f037806a5eedd23c8a9a9bd0
deathcap <deathcap@gmx.co.uk>
2014-01-04 17:34:03 -0800

    Add user input (send chat)

--- examples/mcwebchat/index.js
+++ examples/mcwebchat/index.js
@@ -8,18 +8,33 @@ ever(ws).on('open', function() {
   console.log('opened');
   //ws.send('hey'); // TODO: user input
 });
+
+var outputNode = document.getElementById('output');
+var inputNode = document.getElementById('input');
+
 ever(ws).on('message', function(event, flags) {
   var packet = JSON.parse(event.data);
   var name = packet[0], payload = packet[1];
   
   if (name === 'chat') {
     console.log(payload);
-    console.log('m=',payload.message);
 
-    if (payload.message)
-      document.body.appendChild(tellraw2dom(payload.message));
-      document.body.appendChild(document.createElement('br'));
+    if (payload.message) {
+      outputNode.appendChild(tellraw2dom(payload.message));
+      try {
+        outputNode.appendChild(document.createElement('br'));
+      } catch (error) {
+        outputNode.appendChild(document.createTextNode('error: ' + error));
+      }
+    }
   }
 });
 
-document.write('hi');
+ever(document.body).on('keyup', function(event) {
+  if (event.keyCode !== 13) return;
+
+  var input = inputNode.value;
+  ws.send(JSON.stringify(['chat_message', {message: input}])); // just send it raw
+  
+  inputNode.value = '';
+});
