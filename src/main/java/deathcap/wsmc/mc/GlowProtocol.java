0688b5ea61d09c766b54c6f36d0168644d9fd648
deathcap <deathcap@gmx.co.uk>
2014-06-16 20:48:12 -0700

    Include only header reading/writing from GlowProtocol

--- src/main/java/deathcap/wsmc/mc/GlowProtocol.java
+++ src/main/java/deathcap/wsmc/mc/GlowProtocol.java
@@ -25,50 +25,17 @@
 package deathcap.wsmc.mc;
 
 import com.flowpowered.networking.Codec;
-import com.flowpowered.networking.Message;
-import com.flowpowered.networking.MessageHandler;
-import com.flowpowered.networking.exception.IllegalOpcodeException;
+
 import com.flowpowered.networking.exception.UnknownPacketException;
-import com.flowpowered.networking.protocol.keyed.KeyedProtocol;
-import com.flowpowered.networking.service.CodecLookupService;
 import com.flowpowered.networking.util.ByteBufUtils;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import net.glowstone.GlowServer;
 
 import java.io.IOException;
 
-public abstract class GlowProtocol extends KeyedProtocol {
+public class GlowProtocol {
 
-    /**
-     * Keys for codec lookup.
-     */
-    private static final String INBOUND = "INBOUND";
-    private static final String OUTBOUND = "OUTBOUND";
-
-    public GlowProtocol(GlowServer server, String name, int highestOpcode) {
-        super(name, highestOpcode + 1);
-    }
-
-    protected <M extends Message, C extends Codec<? super M>, H extends MessageHandler<?, ? super M>> Codec.CodecRegistration inbound(int opcode, Class<M> message, Class<C> codec, Class<H> handler) {
-        return registerMessage(INBOUND, message, codec, handler, opcode);
-    }
-
-    protected <M extends Message, C extends Codec<? super M>> Codec.CodecRegistration outbound(int opcode, Class<M> message, Class<C> codec) {
-        return registerMessage(OUTBOUND, message, codec, null, opcode);
-    }
-
-    @Override
-    public <M extends Message> MessageHandler<?, M> getMessageHandle(Class<M> clazz) {
-        MessageHandler<?, M> handler = getHandlerLookupService(INBOUND).find(clazz);
-        if (handler == null) {
-            GlowServer.logger.warning("No message handler for: " + clazz.getSimpleName() + " in " + getName());
-        }
-        return handler;
-    }
-
-    @Override
-    public Codec<?> readHeader(ByteBuf buf) throws UnknownPacketException {
+    public void readHeader(ByteBuf buf) throws UnknownPacketException {
         int length = -1;
         int opcode = -1;
         try {
@@ -78,35 +45,17 @@ public abstract class GlowProtocol extends KeyedProtocol {
             buf.markReaderIndex();
 
             opcode = ByteBufUtils.readVarInt(buf);
-            return getCodecLookupService(INBOUND).find(opcode);
+            //return getCodecLookupService(INBOUND).find(opcode);
         } catch (IOException e) {
             throw new UnknownPacketException("Failed to read packet data (corrupt?)", opcode, length);
-        } catch (IllegalOpcodeException e) {
-            // go back to before opcode, so that skipping length doesn't skip too much
-            buf.resetReaderIndex();
-            throw new UnknownPacketException("Opcode received is not a registered codec on the server!", opcode, length);
         }
     }
 
-    @Override
-    public <M extends Message> Codec.CodecRegistration getCodecRegistration(Class<M> clazz) {
-        CodecLookupService service = getCodecLookupService(OUTBOUND);
-        if (service != null) {
-            Codec.CodecRegistration reg = service.find(clazz);
-            if (reg != null) {
-                return reg;
-            }
-        }
-        GlowServer.logger.warning("No codec to write: " + clazz.getSimpleName() + " in " + getName());
-        return null;
-    }
-
-    @Override
-    public ByteBuf writeHeader(ByteBuf out, Codec.CodecRegistration codec, ByteBuf data) {
+    public ByteBuf writeHeader(ByteBuf out, int opcode, ByteBuf data) {
         final ByteBuf opcodeBuffer = Unpooled.buffer();
-        ByteBufUtils.writeVarInt(opcodeBuffer, codec.getOpcode());
+        ByteBufUtils.writeVarInt(opcodeBuffer, opcode);
         ByteBufUtils.writeVarInt(out, opcodeBuffer.readableBytes() + data.readableBytes());
-        ByteBufUtils.writeVarInt(out, codec.getOpcode());
+        ByteBufUtils.writeVarInt(out, opcode);
         return out;
     }
 
