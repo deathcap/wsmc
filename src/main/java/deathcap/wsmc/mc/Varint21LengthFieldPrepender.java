74997de1bb1ea8c380880287bd355a543696e299
deathcap <deathcap@gmx.co.uk>
2014-06-17 09:52:12 -0700

    Add Varint21FrameDecoder ByteToMessageDecoder from BungeeCord

---
+++ src/main/java/deathcap/wsmc/mc/Varint21LengthFieldPrepender.java
@@ -0,0 +1,74 @@
+/* based on https://github.com/SpigotMC/BungeeCord/tree/master/protocol
+Copyright (c) 2012, md_5. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+The name of the author may not be used to endorse or promote products derived
+from this software without specific prior written permission.
+
+You may not use the software for commercial software hosting services without
+written permission from the author.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+ */
+package deathcap.wsmc.mc;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+@ChannelHandler.Sharable
+public class Varint21LengthFieldPrepender extends MessageToByteEncoder<ByteBuf>
+{
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception
+    {
+        int bodyLen = msg.readableBytes();
+        int headerLen = varintSize( bodyLen );
+        out.ensureWritable( headerLen + bodyLen );
+
+        DefinedPacket.writeVarInt( bodyLen, out );
+        out.writeBytes( msg );
+    }
+
+    private static int varintSize(int paramInt)
+    {
+        if ( ( paramInt & 0xFFFFFF80 ) == 0 )
+        {
+            return 1;
+        }
+        if ( ( paramInt & 0xFFFFC000 ) == 0 )
+        {
+            return 2;
+        }
+        if ( ( paramInt & 0xFFE00000 ) == 0 )
+        {
+            return 3;
+        }
+        if ( ( paramInt & 0xF0000000 ) == 0 )
+        {
+            return 4;
+        }
+        return 5;
+    }
+}
