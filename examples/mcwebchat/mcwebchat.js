90c8db9db6a5673636946384fc25aee6955ca2ac
deathcap <deathcap@gmx.co.uk>
2014-06-23 21:08:16 -0700

    Change example client to use mineflayer
    Requires deathcap/mineflayer@f06dfc9d0b8432e4b23a958c86858005b1dee1d6

--- examples/mcwebchat/mcwebchat.js
+++ examples/mcwebchat/mcwebchat.js
@@ -1,3 +1,5 @@
+var mineflayer = require('mineflayer');
+
 var websocket_stream = require('websocket-stream');
 var minecraft_protocol = require('minecraft-protocol');
 var tellraw2dom = require('tellraw2dom');
@@ -10,27 +12,24 @@ var log = function(s) {
   outputNode.appendChild(document.createElement('br'));
 }
 
-var ws = websocket_stream('ws://localhost:24444/server', {type: Uint8Array});
-console.log('ws',ws);
 // login credential
-// (note: send username, as buffer so binary - new Buffer())
-
+var username;
 var hash = document.location.hash;
 if (hash.length < 2) {
   // try anonymous auth
-  ws.write(new Buffer('mcwebchatuser'));
+  username = 'mcwebchatuserX';
 } else {
-  hash = hash.substring(1); // remove #
-  ws.write(new Buffer(hash)); // credential
+  username = hash.substring(1); // remove #
 }
 
-/*
-ws.addEventListener('open', function() {
-  log('Successfully connected to WebSocket');
+var bot = mineflayer.createBot({
+  username: username
 });
-*/
+bot.on('chat', function(username, message) {
+  log('<'+username+'> '+message); // TODO: change to tellraw2dom raw message, instead of getting preparsed (strips colors, etc.)
+});
 
-ws.on('error', function(exception) {
+bot.on('error', function(exception) {
   console.log(exception);
   if (exception.currentTarget)
     log('WebSocket error connecting to ' + exception.currentTarget.URL);
@@ -38,85 +37,17 @@ ws.on('error', function(exception) {
     log('WebSocket error: ' + exception);
 });
 
-ws.on('close', function() {
+bot.on('close', function() {
   console.log('WebSocket closed');
-});
-
-// decode the binary packet
-var decodePacket = function(data) {
-  if (!(data instanceof Uint8Array)) {
-    return undefined;
-  }
-
-  // convert typed array to NodeJS buffer for minecraft-protocol's API
-  // TODO: is this conversion fast? backed by ArrayBuffer in Browserify 3, see https://npmjs.org/package/native-buffer-browserify
-  //  but is this the right way to "convert" from an ArrayBuffer to a Buffer, without copying?
-  data._isBuffer = true;
-  //var length = String.fromCharCode(data.length); // XXX
-  //var buffer = new Buffer(length + data);
-  var buffer = new Buffer(data);
-
-  var state = minecraft_protocol.protocol.states.PLAY;
-  var isServer = false;
-  var shouldParsePayload = {packet: 1}; // somehow this is needed to parse the fields TODO: figure out how this is supposed to work
-
-  var result = minecraft_protocol.protocol.parsePacket(buffer, state, isServer, shouldParsePayload);
-  if (!result || result.error) {
-    console.log('protocol parse error',result);
-    log('protocol parse error: ' + JSON.stringify(result));
-    return;
-  }
-  var payload = result.results;
-  var id = result.results.id;
-  var name = minecraft_protocol.protocol.packetNames[state].toClient[id];
-  //console.log('parsed',name,result);
-
-  return {name:name, id:id, payload:payload};
-};
-
-var handlePacket = function(id, name, payload) {
-  // show formatted chat
-  if (name === 'chat') {
-    console.log(payload);
-
-    if (payload.message) {
-      outputNode.appendChild(tellraw2dom(payload.message));
-      try {
-        outputNode.appendChild(document.createElement('br'));
-      } catch (error) {
-        outputNode.appendChild(document.createTextNode('error: ' + error));
-      }
-    }
-  // log a few other interesting messages
-  } else if (name === 'disconnect') {
-    log('Kicked by server! Reason: ' + payload.reason);
-  } else if (name === 'spawn_position') {
-    log('Spawned at ('+payload.x+','+payload.y+','+payload.z+')');
-  } else if (name === 'player_info') {
-    if (payload.online)
-      log('Ping: ' + payload.playerName + ' ('+payload.ping+' ms)');
-  } else if (name === 'custom_payload') {
-    if (payload.channel === "MC|Brand") {
-      log('Connected to a ' + payload.data.toString() + ' server');
-    }
-  }
-};
-
-ws.on('data', function(data) {
-  var packet = decodePacket(data);
-  if (!packet) return;
-
-  handlePacket(packet.id, packet.name, packet.payload);
 });
 
 document.body.addEventListener('keyup', function(event) {
   if (event.keyCode !== 13) return;
 
   var input = inputNode.value;
-  var data = minecraft_protocol.protocol.createPacketBuffer('chat', {message: input});
-  console.log('sending data',data);
+  console.log('sending input',input);
 
-  ws.write(data);
+  bot.chat(input);
   
   inputNode.value = '';
 });
