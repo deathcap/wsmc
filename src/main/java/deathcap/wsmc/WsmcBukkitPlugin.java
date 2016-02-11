44639fd689942e5193bc386f40d8df537ceb16e3
deathcap <deathcap@gmx.co.uk>
2016-02-10 22:43:31 -0800

    [wsmc/Java] Refactor Bukkit configuration loading

--- src/main/java/deathcap/wsmc/WsmcBukkitPlugin.java
+++ src/main/java/deathcap/wsmc/WsmcBukkitPlugin.java
@@ -21,16 +21,27 @@ public class WsmcBukkitPlugin extends JavaPlugin implements Listener {
         final FileConfiguration config = getConfig();
         config.options().copyDefaults(true);
 
-        config.addDefault("verbose", new Boolean(true));
-        config.addDefault("websocket.bind-address", "");
-        config.addDefault("websocket.bind-port", 24444);
-        config.addDefault("websocket.external-scheme", "http");
-        config.addDefault("websocket.external-domain", "");
-        config.addDefault("websocket.external-port", 24444);
-        config.addDefault("minecraft.connect-address", "localhost");
-        config.addDefault("minecraft.connect-port", Bukkit.getServer().getPort());
-        config.addDefault("minecraft.announce-on-join", true);
-        config.addDefault("minecraft.allow-anonymous", false);
+        boolean verbose = true;
+        String websocket_bind_address = "";
+        int websocket_bind_port = 24444;
+        String websocket_external_scheme = "http";
+        String websocket_external_domain = "";
+        int websocket_external_port = 24444;
+        String minecraft_connect_address = "localhost";
+        int minecraft_connect_port = Bukkit.getServer().getPort();
+        boolean minecraft_announce_on_join = true;
+        boolean minecraft_allow_anonymous = false;
+
+        config.addDefault("verbose", new Boolean(verbose));
+        config.addDefault("websocket.bind-address", websocket_bind_address);
+        config.addDefault("websocket.bind-port", websocket_bind_port);
+        config.addDefault("websocket.external-scheme", websocket_external_scheme);
+        config.addDefault("websocket.external-domain", websocket_external_domain);
+        config.addDefault("websocket.external-port", websocket_external_port);
+        config.addDefault("minecraft.connect-address", minecraft_connect_address);
+        config.addDefault("minecraft.connect-port", minecraft_connect_port);
+        config.addDefault("minecraft.announce-on-join", minecraft_announce_on_join);
+        config.addDefault("minecraft.allow-anonymous", minecraft_allow_anonymous);
         config.addDefault("filter.whitelist", new Integer[] { }); // TODO: each direction
         config.addDefault("filter.blacklist", new Integer[] { });
 
@@ -43,17 +54,28 @@ public class WsmcBukkitPlugin extends JavaPlugin implements Listener {
             this.getConfig().set("websocket.external-domain", ExternalNetworkAddressChecker.getHostName());
         }
 
+        verbose = this.getConfig().getBoolean("verbose");
+        websocket_bind_address = this.getConfig().getString("websocket.bind-address");
+        websocket_bind_port = this.getConfig().getInt("websocket.bind-port");
+        websocket_external_scheme = this.getConfig().getString("websocket.external-scheme");
+        websocket_external_domain = this.getConfig().getString("websocket.external-domain");
+        websocket_external_port = this.getConfig().getInt("websocket.external-port");
+        minecraft_connect_address = this.getConfig().getString("minecraft.connect-address");
+        minecraft_connect_port = this.getConfig().getInt("minecraft.connect-port");
+        minecraft_announce_on_join = this.getConfig().getBoolean("minecraft.announce-on-join");
+        minecraft_allow_anonymous = this.getConfig().getBoolean("minecraft.allow-anonymous");
+
         saveConfig();
 
-        String url = this.getConfig().getString("websocket.external-scheme")
+        String url = websocket_external_scheme
                 + "://"
-                + this.getConfig().getString("websocket.external-domain")
-                + (this.getConfig().getInt("websocket.external-port") != 80
-                    ? (":" + this.getConfig().getInt("websocket.external-port")) : "")
+                + websocket_external_domain
+                + (websocket_external_port != 80
+                    ? (":" + websocket_external_port) : "")
                 + "/";
         users = new UserIdentityLinker(url,
-                this.getConfig().getBoolean("minecraft.announce-on-join"),
-                this.getConfig().getBoolean("minecraft.allow-anonymous"),
+                minecraft_announce_on_join,
+                minecraft_allow_anonymous,
                 this);
         getServer().getPluginManager().registerEvents(users, this);
         getCommand("web").setExecutor(users);
@@ -64,12 +86,12 @@ public class WsmcBukkitPlugin extends JavaPlugin implements Listener {
 
 
         webThread = new WebThread(
-                this.getConfig().getString("websocket.bind-address"),
-                this.getConfig().getInt("websocket.bind-port"),
-                this.getConfig().getString("minecraft.connect-address"),
-                this.getConfig().getInt("minecraft.connect-port"),
+                websocket_bind_address,
+                websocket_bind_port,
+                minecraft_connect_address,
+                minecraft_connect_port,
                 users, filter,
-                this.getConfig().getBoolean("verbose")
+                verbose
                 );
         webThread.start();
     }
